cfe3d12e2f3e5fb19dd40e8fc2c753459573ca85
v_gaoruiqiang <v_gaoruiqiang@baidu.com>
2019-05-19 12:10:01 +0800

    idea整合git测试

---
+++ src/main/java/com/weixinpay/demo/control/Hello.java
@@ -0,0 +1,25 @@
+package com.weixinpay.demo.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author gaoruiqiang
+ * @Description
+ * @Date:19
+ *//*
+ @author: gaoruiqiang
+ @DESCRIPTION:
+ @create:2019/5/19
+*/
+
+@Controller
+public class Hello {
+
+        @RequestMapping("/hello")
+        public String hello(){
+
+        return "hello grq";
+    }
+
+}
