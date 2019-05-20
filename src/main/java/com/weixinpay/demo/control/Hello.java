71f1ba99a9a9df296f6a703619bbfa0f42351244
v_gaoruiqiang <v_gaoruiqiang@baidu.com>
2019-05-20 18:28:43 +0800

    配置文件修改

--- src/main/java/com/weixinpay/demo/control/Hello.java
+++ src/main/java/com/weixinpay/demo/control/Hello.java
@@ -21,7 +21,7 @@ public class Hello {
         @RequestMapping("/hello")
         public String hello(){
         //测试
-        return "hello grq";
+        return "templates/index.jsp";
     }
 
 }
