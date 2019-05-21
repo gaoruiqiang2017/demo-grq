286040dbfc5ea816341fe640636122d995377e48
v_gaoruiqiang <v_gaoruiqiang@baidu.com>
2019-05-21 17:10:53 +0800

    配置文件修改

--- src/main/java/com/weixinpay/demo/DemoApplication.java
+++ src/main/java/com/weixinpay/demo/DemoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = "com.weixinpay.demo")
 public class DemoApplication extends SpringBootServletInitializer implements
         WebApplicationInitializer {
 
